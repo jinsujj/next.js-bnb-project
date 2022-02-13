50ac4fd50c11ac0e4f0df444fe4b79a86c04cd77
jinsujj <wlstncjs1234@naver.com>
2022-02-13 23:21:05 +0900

    aws-sdk로 파일 업로드하기

--- pages/api/files/upload.ts
+++ pages/api/files/upload.ts
@@ -1,6 +1,9 @@
-import formidable from "formidable";
+import formidable, { FileJSON } from "formidable";
 import { NextApiRequest, NextApiResponse } from "next";
-
+import aws from "aws-sdk";
+import { createReadStream } from "fs";
+import { rejects } from "assert";
+import {v4 as uuidv4} from "uuid";
 
 export const config = {
     api: {
@@ -8,20 +11,44 @@ export const config = {
     }
 };
 
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         try {
             const form = new formidable.IncomingForm();
-            form.parse(req, async (err, fields, files) => {
-                console.log(files);
+
+            const url = await new Promise((resolve, reject) => {
+                form.parse(req, async (err, fields, files) => {
+                    const s3 = new aws.S3({
+                        accessKeyId: process.env.ACCESSSKEY_ID,
+                        secretAccessKey: process.env.SECRET_ACCESSKEY_ID,
+                    });
+                    var formidableFile:any = files.file;
+                    const stream = createReadStream(formidableFile.filepath);
+
+                    // 파일 이름
+                    const originalFileName = formidableFile.originalFilename.split(".").shift();
+                    // 확장자
+                    const fileExtension = formidableFile.originalFilename.split(".").pop();
+
+                    await s3.upload({
+                        Bucket: process.env.S3_BUCKET_NAME!,
+                        Key: `${originalFileName}__${uuidv4()}.${fileExtension}`,
+                        ACL: "public-read",
+                        Body: stream,
+                    })
+                        .promise()
+                        .then((res) => resolve(res.Location))
+                        .catch((e) => rejects(e));
+                });
             });
-        } catch(e){
+            res.statusCode = 201;
+            res.send(url);
+        } catch (e) {
             console.log(e);
             res.end();
         }
-    }
-    res.statusCode = 405;
+        res.statusCode = 405;
 
-    return res.end();
+        return res.end();
+    }
 }
